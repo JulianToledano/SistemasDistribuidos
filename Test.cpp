f3cdd61472408984ffea3fc080a828ef20e37178
Julian Toledano <jtoledanodiaz@gmail.com>
2016-11-11 20:04:48 +0100

    Primer test.

--- Test.cpp
+++ Test.cpp
@@ -1,6 +1,29 @@
-
-
+#include "Arbol.h"
+#include <iostream>
 int main(){
 
+  Arbol *test = new Arbol();
+
+  test->insertarNodo("tmp", true);
+  test->insertarNodo("bin", true);
+  test->insertarNodo("urs", true);
+  test->insertarNodo("ttt", true);
+
+  std::cout << "SIN MODIFICACION\n";
+  for(int i = 0; i < test->getRoot()->getHijos()->size(); i++)
+    std::cout << test->getRoot()->getHijos()->at(i)->getNombre() << std::endl;
+
+  std::cout << "BUSCANDO EL NODO: ";
+  Nodo* buscado = test->buscarNodo("tmp");
+  std::cout << buscado->getNombre() << std::endl;
+
+  std::cout << "CAMBIAMOS EL NOMBRE DESDE EL NODO ENCONTRADO: ";
+  buscado->setNombre("chg");
+  for(int i = 0; i < test->getRoot()->getHijos()->size(); i++)
+    std::cout << test->getRoot()->getHijos()->at(i)->getNombre() << std::endl;
+
+  std::cout << "no error" << std::endl;
+
+  delete test;
   return 0;
 }
