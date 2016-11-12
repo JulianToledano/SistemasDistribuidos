e93336b7f3c5c90217c1654cebaafd028e112829
Julian Toledano <jtoledanodiaz@gmail.com>
2016-11-12 01:56:11 +0100

    Prueba eliminar Nodo

--- Test.cpp
+++ Test.cpp
@@ -12,18 +12,24 @@ int main(){
   std::cout << "SIN MODIFICACION\n";
   for(int i = 0; i < test->getRoot()->getHijos()->size(); i++)
     std::cout << test->getRoot()->getHijos()->at(i)->getNombre() << std::endl;
+  std::cout << "Tamano del vecto: " << test->getRoot()->getHijos()->size() << std::endl;
 
   std::cout << "BUSCANDO EL NODO: ";
   Nodo* buscado = test->buscarNodo("tmp");
   std::cout << buscado->getNombre() << std::endl;
+  std::cout << "Tamano del vecto: " << test->getRoot()->getHijos()->size() << std::endl;
 
-  std::cout << "CAMBIAMOS EL NOMBRE DESDE EL NODO ENCONTRADO: ";
+  std::cout << "CAMBIAMOS EL NOMBRE DESDE EL NODO ENCONTRADO:\n";
   buscado->setNombre("chg");
   for(int i = 0; i < test->getRoot()->getHijos()->size(); i++)
     std::cout << test->getRoot()->getHijos()->at(i)->getNombre() << std::endl;
+  std::cout << "Tamano del vecto: " << test->getRoot()->getHijos()->size() << std::endl;
 
-  std::cout << "no error" << std::endl;
-
+  std::cout << "ELIMINAR EL NODO: bin\n";
+  test->eliminarNodo("bin");
+  for(int i = 0; i < test->getRoot()->getHijos()->size(); i++)
+    std::cout << test->getRoot()->getHijos()->at(i)->getNombre() << std::endl;
+  std::cout << "Tamano del vecto: " << test->getRoot()->getHijos()->size() << std::endl;
   delete test;
   return 0;
 }
